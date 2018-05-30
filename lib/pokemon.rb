ae1fc4cee6778b109762be73f4a04c879e657bae
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-05-30 20:36:20 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -8,6 +8,6 @@ class Pokemon
   end
 
   def self.save(name, type, database_connection)
-    database_connection.execute("INSERT INTO Pokemon (name, type) VALUES (?, ?)",name, type)
+    database_connection.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)",name, type)
   end
 end
